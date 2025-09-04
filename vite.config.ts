68f22a22cced2c83ffc03d7e39fc734455b8d7f7
Muhammad_Junaid <junaidsabir2848@gmail.com>
2025-09-04 11:06:45 +0500

    prepared for github live

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+//import { componentTagger } from "lovable-tagger";
+//import react from '@vitejs/plugin-react';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,12 +13,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/Beauty_Salon_Project/', // 👈 IMPORTANT
 }));
+
